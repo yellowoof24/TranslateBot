1fb607f5c349994d497ad7a928ba0a79f5ac49f4
노랑끄투 <72529713+yellowoof24@users.noreply.github.com>
2021-06-12 10:52:17 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', msg => {
+	function translate(str) {
+		axios
+			.get(
+				'https://dapi.kakao.com/v2/translation/translate?src_lang=kr&target_lang=en',
+				{
+					params: {
+						query: str
+					},
+					headers: {
+						Authorization: 'KakaoAK ab04bb31e63f1cfa2dab2274f281a38f'
+					}
+				}
+			)
+			.then(function(response) {
+				msg.reply(response.data.translated_text[0][0]);
+			})
+			.catch(function(error) {
+				msg.reply('번역 실패');
+			});
+	}
+
+	if (msg.content.startsWith('!번역')) {
+		const a = msg.content.split(' ')[1];
+		if (a) {
+			translate(a);
+		} else {
+			msg.reply('번역할 글을 넣어주세요');
+		}
+	}
+});
+
+client.login('');
